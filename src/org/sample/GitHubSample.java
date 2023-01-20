a04f83005a80d51d1ee4e10a51d254cc12de40af
AV5600 <yupitsav56@gmail.com>
2023-01-20 17:45:26 +0530

    Done

--- src/org/sample/GitHubSample.java
+++ src/org/sample/GitHubSample.java
@@ -5,11 +5,14 @@ public class GitHubSample {
 	public static void main(String[] args) {
 		
 		System.out.println("   *   ");
-		System.out.println("  * *   ");
-		System.out.println(" *   *  ");
-		System.out.println("*     * ");
-		System.out.println(" *   *  ");
-		System.out.println("  * *   ");
+		System.out.println("  * *  ");
+		System.out.println(" *   * ");
+		System.out.println("*     *");
+		System.out.println(" *   * ");
+		System.out.println("  * *  ");
 		System.out.println("   *   ");
+		System.out.println("Good Job");
+		
+		
 	}
 }
