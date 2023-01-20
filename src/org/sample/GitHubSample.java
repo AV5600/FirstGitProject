af5eab100b7638699d1a070671dd484d4ed0490b
AV5600 <yupitsav56@gmail.com>
2023-01-20 17:04:54 +0530

    GoodJob

---
+++ src/org/sample/GitHubSample.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class GitHubSample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("   *   ");
+		System.out.println("  * *   ");
+		System.out.println(" *   *  ");
+		System.out.println("*     * ");
+		System.out.println(" *   *  ");
+		System.out.println("  * *   ");
+		System.out.println("   *   ");
+	}
+}
